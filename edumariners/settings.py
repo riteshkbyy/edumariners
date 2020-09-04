fad1a0ab0398c1b3c76f82c3711e54f4ee260e29
RITESH YADAV <riteshkbyy@gmail.com>
2020-09-04 20:32:55 +0530

    Update settings.py

--- edumariners/settings.py
+++ edumariners/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ya2a8t7umt-lw)@^=r%xt7$81yt3wj#!evaocf5l1f-xgezx=c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
